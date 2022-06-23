280ec2a59820bdb7b65a5b030b3aacb7568ca5fa
Jonah Grimes <jgrimes@launchdarkly.com>
2022-06-23 16:21:49 -0400

    corrected type name in useFlagVariation

--- src/support/useFlagVariation.ts
+++ src/support/useFlagVariation.ts
@@ -2,9 +2,9 @@ import type { LDClient } from "launchdarkly-js-client-sdk"
 import { useLDClient } from "launchdarkly-react-client-sdk"
 
 type Optional<T> = T | undefined
-type UseVariation = <T>(name: string, defaultValue?: T) => T
+type UseFlagVariation = <T>(name: string, defaultValue?: T) => T
 
-const useFlagVariation: UseVariation = <T>(name: string, defaultValue?: T) => {
+const useFlagVariation: UseFlagVariation = <T>(name: string, defaultValue?: T) => {
     const ldClient: Optional<LDClient> = useLDClient()
     if (ldClient) return ldClient.variation(name, defaultValue)
     return defaultValue
